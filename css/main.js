e4b7382ca95d96682885ad4118bea88083975fca
rgmanikandan07 <rgmanimani0707@gmail.com>
2022-10-10 15:00:29 +0530

    add new file

---
+++ css/main.js
@@ -0,0 +1,21 @@
+// function greeting(name) {
+//     alert(`Hello, ${name}`);
+//   }
+  
+//   function processUserInput(callback) {
+//     const name = prompt("Please enter your name.");
+//     callback(name);
+//   }
+  
+//   processUserInput(greeting);;
+
+
+// function abc(name){
+//     alert("hello",name);
+// }
+
+// function input(callback){
+//     const name = ("enter name")
+//         callback(name);
+// }
+// input(abc);
